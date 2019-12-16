fb4b67a1ef0f409473bff273523ab4ab8ccbbd56
Notesong <notesong.git@outlook.com>
2019-12-15 21:39:24 -0500

    Add axios call and character card with formatting

--- src/components/CharacterCard.js
+++ src/components/CharacterCard.js
@@ -1,5 +1,44 @@
 import React from "react";
+import styled from "styled-components";
 
-export default function CharacterCard() {
-  return <span>todo: character</span>;
+const Card = styled.div`
+  display: flex;
+  flex-flow: col wrap;
+  margin 1%;
+  width: 48%;
+  background: #fff;
+  box-shadow: 0 19px 38px rgba(0, 0, 0, 0.3), 0 15px 12px rgba(0, 0, 0, 0.22);
+
+  @media only screen and (max-width: 800px) {
+    width: 98%
+  }
+`;
+
+const CardBody = styled.div`
+  padding: 2rem;
+`;
+
+const CardImage = styled.div`
+  width: 40%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+const Image = styled.img`
+  height: 80%;
+`;
+
+export default function CharacterCard({ character }) {
+  return (
+    <Card>
+      <CardImage>
+        <Image src={character.image} alt={character.name} />
+      </CardImage>
+      <CardBody>
+        <h3>{character.name}</h3>
+        <p>Orgin: {character.origin.name}</p>
+      </CardBody>
+    </Card>
+  );
 }
--- src/components/CharacterList.js
+++ src/components/CharacterList.js
@@ -1,16 +1,28 @@
 import React, { useEffect, useState } from "react";
+import axios from 'axios';
+import CharacterCard from "./CharacterCard";
 
 export default function CharacterList() {
-  // TODO: Add useState to track data from useEffect
+  const [characterList, setCharacterList] = useState([]);
 
   useEffect(() => {
-    // TODO: Add API Request here - must run in `useEffect`
-    //  Important: verify the 2nd `useEffect` parameter: the dependancies array!
+    axios
+      .get(`https://rickandmortyapi.com/api/character/`)
+      .then(response => {
+        const charList = response.data.results;
+        console.log(charList);
+        setCharacterList(charList);
+      })
+      .catch(error => {
+        console.log("The data was not returned", error);
+      })
   }, []);
 
   return (
     <section className="character-list">
-      <h2>TODO: `array.map()` over your state here!</h2>
+      {characterList.map(character => {
+        return <CharacterCard key={character.id} character={character} />
+      })}
     </section>
   );
 }
